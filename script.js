f07df88b2672967ef2be4319147ca3dc609ba4b9
sa20122424 <saloxiddin201224@gmail.com>
2024-08-11 20:16:46 +0500

    salom

---
+++ script.js
@@ -0,0 +1,123 @@
+
+
+// let day = +prompt("son kiriting");
+
+
+// switch (day) {
+//     case 1:
+//         alert('Monday');
+//         break;
+//     case 2:
+//         alert('Tuesday');
+//         break;
+//     case 3:
+//         alert('Wednesday');
+//         break;
+//     case 4:
+//         alert('Thursday');
+//         break;
+//     case 5:
+//         alert('Friday');
+//         break;
+//     case 6:
+//         alert('Saturday');
+//         break;
+//     case 7:
+//        alert('Sunday');
+//         break;
+//     default:
+//         alert('Invalid day');
+// }
+
+
+// let odam = +prompt("nomer kiriting");
+
+
+// switch (odam) {
+//     case 101:
+//         alert(' телефонный номер пожарной службы в России, Белоруссии и Украине.');
+//         break;
+//     case 102:
+//         alert('внутренних дел для оказания оперативной помощи населению.');
+//         break;
+//     case 103:
+//         alert('скорую помощь');
+//         break;
+//     case 104:
+//         alert(' аварийная служба газовой сети');
+//         break;
+//     case 1005:
+//         alert('Служба спасения ');
+//         break;
+//     case 1007:
+//         alert('с тобой происходит работа на тонком плане');
+//         break;
+//     case +998946187788:
+//        alert('emirhan');
+//         break;
+//     default:
+//         alert('Invalid nomer');
+// }
+
+
+
+// let salom = prompt("привет")
+
+// let salom1 = prompt("скок лет ")
+
+// let salom2 = prompt("где живеш ")
+
+
+
+// switch (salom) {
+//     case    :
+//         alert('volekum assalom')
+
+//         break;
+//      case 18:
+//         alert('ok')
+//         break;
+//     case yunsobod:
+//         alert("bildim")
+//         break;
+
+//       default:
+//         alert('hayr')
+//         break;
+// }
+
+let olma = prompt("напишить овощь или фрукт")
+
+switch (olma) {
+   
+    case 'морковь':
+        case 'редис':
+        case 'капуста':
+        case 'помидор':
+        case 'свёкла':
+        case 'лук':
+        case 'бобы':
+        case 'горох': 
+        case 'огурец':
+        case 'укроп':
+            alert('овощь')
+        break;
+        case 'яблока':
+        case 'банана':
+        case 'апельсин':
+        case 'ананас':
+        case 'груша':
+        case 'вишня':
+        case 'черешня':
+        case 'лимон': 
+        case 'яблоня':
+        case 'мандарин':
+        
+        alert('фрукт')
+        break;
+
+    default:
+        alert('chiragan')
+        break;
+}
+
